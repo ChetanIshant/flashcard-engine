d1cf9fd8f3ce2de2d3d836a818dcb87db2d831fa
Chetan <ishunara25@gmail.com>
2026-08-14 15:53:51 +0530

    Add main check to run app locally with uvicorn

--- dev.py
+++ dev.py
@@ -11,6 +11,9 @@ app.mount("/js", StaticFiles(directory="public/js"), name="js")
 async def root():
     return FileResponse("public/index.html")
 
+if __name__ == "__main__":
+    print("Running Flashcard Engine locally at http://127.0.0.1:8000")
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
 #
