f4ef90798550c76098219d7047e17676b51de346
Chetan <ishunara25@gmail.com>
2026-08-14 15:54:10 +0530

    Add imports for FastAPI and Uvicorn

--- dev.py
+++ dev.py
@@ -1,4 +1,7 @@
-
+import uvicorn
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from api.index import app
 
 # Mount static directories for local development
 app.mount("/css", StaticFiles(directory="public/css"), name="css")
